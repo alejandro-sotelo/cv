5d2dbf8c42a0830f2b3aeba8402675848eef71fa
Alejandro Sotelo <alejandrosotelo.ie@gmail.com>
2019-10-20 21:29:04 -0500

    first commit

---
+++ js/util.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+var myFullpage = new fullpage('#fullpage', {
+    licenseKey: 'null',
+    sectionsColor: ['#14022C', '#C7CDD0', '#14022C', '#C7CDD0', '#14022C', '#C7CDD0', '#14022C']
+});
